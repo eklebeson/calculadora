50a86b07b5f8138f80ce267e4aac2255c0d2937f
eklebeson <eklebeson@gmail.com>
2023-03-31 09:53:11 -0300

    first commit

---
+++ src/components/Input/styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 75px;
+  background-color: #aa5afa;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  font-size: 24px;
+  font-family: "Roboto";
+
+  input {
+    width: 100%;
+    height: 75px;
+    background-color: #aa5afa;
+    border: 0;
+    flex-direction: column;
+    alkign-items: flex-end;
+    padding: 0 10px;
+
+    font-size: 24px;
+    font-family: "Roboto";
+    color: #ffffff;
+  }
+`;
